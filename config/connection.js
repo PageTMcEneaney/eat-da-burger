b12c2b04262fd93ac9924f645177fdf5c48a4ce9
PTMcEneaney <pagemceneaney@gmail.com>
2019-07-01 20:37:33 -0700

    add .env file

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,5 @@
 // Set up MySQL connection.
+var JAWSDB_URL = require("./.env")
 var mysql = require("mysql");
 
 var connection;
@@ -8,7 +9,7 @@ if (process.env.JAWSDB_URL) {
 } else {
   connection = mysql.createConnection({
     host: "localhost",
-    // port: 3306,
+    port: 3306,
     user: "root",
     password: "MySQLbau5!",
     database: "burgers_db"
